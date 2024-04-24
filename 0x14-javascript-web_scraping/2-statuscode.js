32da16c51606a31f017297067e505bbbaf4f1537
phylkemboi <philkemboi4576@email.coml>
2024-04-24 15:32:37 -0700

    done

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+const url = process.argv[2];
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const { argv } = require('process');
-
-const BaseUrl = 'https://swapi-api.alx-tools.com/api';
-request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
-  if (error) {
-    console.error(error);
-  }
-  console.log(JSON.parse(body).title);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
